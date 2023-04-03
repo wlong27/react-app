54560cc8401fdc567fc89364a27122753ca5f861
WeiLong <wloh2@its.jnj.com>
2023-04-03 14:02:41 +0800

    updated

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,6 +1,5 @@
 function ListGroup() {
   const items = ["New York", "San Franciso", "Tokyo", "London", "Paris"];
-
   if (items.length === 0) {
     return (
       <>
@@ -10,9 +9,14 @@ function ListGroup() {
     );
   }
 
+  const getMessage = () => {
+    items.length === 0 && <p> No item found</p>;
+  };
+
   return (
     <>
       <h1>List</h1>
+      {getMessage()}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}> {item}</li>
