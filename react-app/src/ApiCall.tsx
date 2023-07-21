4137f2b6ae3f91989b036043af01a49e3a4dd997
WeiLong <wloh2@its.jnj.com>
2023-07-21 19:48:48 +0800

    paginated characters

--- react-app/src/ApiCall.tsx
+++ react-app/src/ApiCall.tsx
@@ -1,39 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from 'axios';
 
 const ApiCall: React.FC = () => {
-    const [data, setData] = useState<Character[]>([]);
-  
-    useEffect(() => {
-      // Function to make the API call
-      const fetchData = async () => {
-        try {
-          const response = await fetch('https://swapi.dev/api/people/');
-          const data = await response.json();
-          setData(data.results); // Store the API response (an array of people) in the 'data' state
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      };
-  
-      fetchData(); // Call the function to fetch data when the component mounts
-    }, []);
-  
-    return (
-        <div>
-          <h1>Star Wars Characters:</h1>
-          <ul>
-            {data.map((character) => (
-              <li key={character.name}>
-                <Link to={`/characters/${encodeURIComponent(character.name)}`}>
-                  {character.name}
-                </Link>
-                - {character.height}cm, {character.mass}kg
-              </li>
-            ))}
-          </ul>
-        </div>
-      );
+
+  const [data, setData] = useState<Character[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://swapi.dev/api/people/?page=${currentPage}`
+        );
+        const data = await response.data.results;
+        setData(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, [currentPage]);
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
   };
-  
+
+  const handlePrevPage = () => {
+    setCurrentPage((prevPage) => prevPage - 1);
+  };
+
+  return (
+    <div>
+      <h1>Star Wars Characters:</h1>
+      <ul>
+        {data.map((character, index) => (
+          <li key={index}>
+            <Link to={`/characters/${encodeURIComponent(character.name)}`}>
+              {character.name}
+            </Link>
+            - {character.height}cm, {character.mass}kg
+          </li>
+        ))}
+      </ul>
+      <button onClick={handlePrevPage} disabled={currentPage === 1}>
+        Previous Page
+      </button>
+      <button onClick={handleNextPage}>Next Page</button>
+    </div>
+
+  );
+};
+
 export default ApiCall;
